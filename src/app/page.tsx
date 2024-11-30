d6f1fed59a78200a03c701659482351f414f9b67
SaadAslam58 <saadxpert58@gmail.com>
2024-11-30 22:26:18 +0500

    fourth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ type Structure = {
   id: number,
   firstName: string,
   lastName: string,
-  age: number
+  age: number,
   email: string,
   address: {
     street: string,
@@ -19,44 +19,38 @@ type Structure = {
   }
 }
 
-const page = () => {
-  const [data, setData] = useState<Structure[]>([])
+const Page = () => {
+  const [data, setData] = useState<Structure[]>([]);
   console.log(data);
-  
-  
-  useEffect(() =>{
+
+  useEffect(() => {
     async function fetchData() {
-    const getData = await fetch("https://dummyjson.com/users")
-    const responce = await getData.json();
-    setData(responce.users);
+      const getData = await fetch("https://dummyjson.com/users");
+      const response = await getData.json();
+      setData(response.users);
     }
     fetchData();
-  },[]);
+  }, []);
+
   return (
     <div className='min-h-screen flex justify-center items-center flex-col w-[100%]'>
-      <h1 className='text-2xl text-black mb-6 text-center content-center cursor-pointer'>Application Programming Interface(API)</h1>
-    <div className='w-[90%] mx-auto grid grid-cols-1 md:grid-cols-3 gap-7 '>
-    {data.slice(1, 10).map((item)=> {
-      return (
-      <div
-      key={item.id}
-      className='border-2  border-gray-400 p-2 rounded-md shadow-md transition-all duration-300 hover:shadow-2xl shadow-gray cursor-pointer'
-      >
-        <div >
-        <h1>{`My name is ${item.firstName} ${item.lastName} and my age is ${item.age} `}</h1>
-        
-        </div>
+      <h1 className='text-2xl text-black mb-6 text-center content-center cursor-pointer'>
+        Application Programming Interface (API)
+      </h1>
+      <div className='w-[90%] mx-auto grid grid-cols-1 md:grid-cols-3 gap-7'>
+        {data.slice(0, 10).map((item) => (
+          <div
+            key={item.id}
+            className='border-2 border-gray-400 p-2 rounded-md shadow-md transition-all duration-300 hover:shadow-2xl shadow-gray cursor-pointer'
+          >
+            <div>
+              <h1>{`My name is ${item.firstName} ${item.lastName} and my age is ${item.age}`}</h1>
+            </div>
+          </div>
+        ))}
       </div>
-       
-      )
-    })}
     </div>
-    </div>
-  )
+  );
 }
 
-export default page
-
-
-
-// "http://simple-books-api.glitch.me/books"
+export default Page;
